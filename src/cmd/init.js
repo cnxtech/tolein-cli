2d5b37e381ae06b6b4441bc56b53327937ffa297
161250053 <161250053@smail.nju.edu.cn>
2019-03-07 16:36:19 +0800

    Fix: No response when project doesn't exist

--- src/cmd/init.js
+++ src/cmd/init.js
@@ -8,17 +8,17 @@ const message = require('../message');
 const spinner = ora('Getting tempalate...');
 module.exports = () => {
     co(function* () {
-        const templateName = yield prompt('template name: ');
+        // const templateName = yield prompt('template name: ');
         const projectName = yield prompt('project name: ');
         return new Promise((resolve, reject) => {
             resolve({
-                templateName,
+                // templateName,
                 projectName
             })
         })
 
     }).then(res => {
-        const { templateName, projectName } = res;
+        const { projectName } = res;
         fs.exists(projectName, (exists) => {
             if (exists) {
                 message.error(`Project '${projectName}' is already exists. Do you want to overwrite it?`);
@@ -27,7 +27,6 @@ module.exports = () => {
                     return new Promise((resolve, reject) => {
                         resolve(overwrite);
                     })
-
                 }).then(overwrite => {
                     if (overwrite.toLowerCase() === 'y') {
                         exec(`rm -rf ${projectName}`, (err) => {
@@ -35,34 +34,40 @@ module.exports = () => {
                                 message.error('Failed to remove the project.');
                                 process.exit();
                             }
-                            const repositoryUrl = `https://github.com/lioder/tolein-templates.git`
-                            const cloneCmd = `git clone ${repositoryUrl} ${projectName}`;
-                            spinner.start();
-                            exec(cloneCmd, (err) => {
-                                if (err) {
-                                    message.error('Error getting template');
-                                    process.exit();
-                                }
-                                const deleteCmd = `cd ${projectName} && rm -rf .git`
-                                exec(deleteCmd, (err) => {
-                                    spinner.stop();
-                                    if (err) {
-                                        message.error('Error getting template');
-                                        process.exit();
-                                    }
-                                    message.success('Template was successfully obtained');
-                                    message.info(`Run "cd ${projectName} && npm install"`);
-                                    process.exit();
-                                })
-                            })
+                            init(projectName);
                         })
                     } else if (overwrite.toLowerCase() === 'n') {
                         message.error('Failed to init an exsting project. Changing your project name may help.');
                         process.exit();
                     }
                 })
+            } else {
+                init(projectName);
             }
         })
 
+    })
+}
+
+function init(projectName) {
+    const repositoryUrl = `https://github.com/lioder/tolein-templates.git`
+    const cloneCmd = `git clone ${repositoryUrl} ${projectName}`;
+    spinner.start();
+    exec(cloneCmd, (err) => {
+        if (err) {
+            message.error('Error getting template');
+            process.exit();
+        }
+        const deleteCmd = `cd ${projectName} && rm -rf .git`
+        exec(deleteCmd, (err) => {
+            spinner.stop();
+            if (err) {
+                message.error('Error getting template');
+                process.exit();
+            }
+            message.success('Template was successfully obtained');
+            message.info(`Run "cd ${projectName} && npm install"`);
+            process.exit();
+        })
     })
 }
